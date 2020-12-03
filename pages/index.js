5d925f5d526564580d3e22ef94d548222dfda5b4
Daymond Blair <mondday@Daymonds-MacBook-Pro.local>
2020-12-03 01:28:05 -0800

    completed game trailers section

--- pages/index.js
+++ pages/index.js
@@ -32,9 +32,11 @@ export default function Home() {
       </header>
       {/* BODY */}
       <div className="container">
+        {/* Game Navigation */}
         <section className="game-nav">
           <div className="game active">
             <i className="fa fa-location-arrow"></i>
+            <img src="" />
           </div>
           <div className="game">
             <div className="img"
@@ -100,16 +102,83 @@ export default function Home() {
             }}></div>
           </div>
         </section>
+        {/* Info */}
         <section className="info">
             <span className="sub-title">
               Official new from Playstation | 12/15/2020
             </span>
             <h1>Ready for a PS5 Adventure?</h1>
             <p>
-              ASTRO'S PLAYROOM is pre-loaded and waiting for you! Explore the new capabilities of PS5 and feel the world...
+              ASTRO'S PLAYROOM is pre-loaded and waiting for you! Explore the new capabilities of PS5 and feel the power of our innovative DualSense techno...
             </p>
         </section>
       </div>
+      {/* Trailers  */}
+      <section className="video-trailers">
+            <div className="trailer">
+              <img className="image" src="https://i.ytimg.com/vi/ZM7HC4aQ57g/maxresdefault.jpg"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">Grand Theft Auto 5</span>
+              </div>
+            </div>
+            <div className="trailer">
+              <img className="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRrchRI8TlWMONswQuaxnjke18k2o2LwPXHA&usqp=CAU"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">God of War</span>
+              </div>
+            </div>
+            <div className="trailer">
+              <img className="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKqkBEhUKhohXLhs6G9wjVzqIui4pzHQnc2w&usqp=CAU"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">Genshin Impact</span>
+              </div>
+            </div>
+            <div className="trailer">
+              <img className="image" src="https://i.pinimg.com/736x/7e/62/46/7e6246028a42ea73c13b1b797d9b8d73.jpg"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">Cuphead 2</span>
+              </div>
+            </div>
+            <div className="trailer">
+              <img className="image" src="https://images.spot.im/v1/production/yakgvcypsno3wrpwqavh"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">Dragon's Dogma</span>
+              </div>
+            </div>
+            <div className="trailer">
+              <img className="image" src="https://cdn.mos.cms.futurecdn.net/pf9rXsPR6gLfArWHkr8odK.jpg"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">Yakuza: Like a Dragon</span>
+              </div>
+            </div>
+            <div className="trailer">
+              <img className="image" src="https://cdn.gamer-network.net/2020/articles/2020-09-13-12-44/godfall-will-be-available-in-three-editions-heres-what-you-get-in-each-one-1599997494896.jpg/EG11/thumbnail/1920x1076/format/jpg/quality/80"/>
+              <div className="video-info">
+                <div className="circle-player">
+                  <i className="fa fa-play"></i>
+                </div>
+                <span className="title">Godfall</span>
+              </div>
+            </div>
+        </section>
     </div>
   )
 }
